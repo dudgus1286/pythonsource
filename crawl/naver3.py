5aee9f4921497b0696579374e4764dd2ad1c6f51
dudgus1286 <dudgus1286@gmail.com>
2024-05-29 14:33:50 +0900

    정규식 표현, 정규식을 이용해 데이터 분류해서 엑셀파일에 저장

--- crawl/naver3.py
+++ crawl/naver3.py
@@ -2,43 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.error import HTTPError
 from openpyxl import Workbook
-import re
 
 wb = Workbook()
 ws = wb.active
 
 ws.title = "네이버open"
 # 너비 조정
-ws.column_dimensions["B"].width = 100
-ws.column_dimensions["C"].width = 60
+ws.column_dimensions["B"].width = 30
+ws.column_dimensions["C"].width = 80
 
-ws.append(["순위", "상품명", "판매경로"])
+ws.append(["번호", "isbn", "책제목", "할인가격"])
 
 headers = {
     "X-Naver-Client-Id": "y75cFyztfRqWH6MqHjz4",
     "X-Naver-Client-Secret": "QtZOXTb6DB",
 }
 
-num, start = 1, 1
-for idx in range(10):
-    # idx: 0~9
+num, start = 0, 1
+for idx in range(2):
     start_num = start + (idx * 100)
-    url = "https://openapi.naver.com/v1/search/shop.json"
+    url = "https://openapi.naver.com/v1/search/book.json"
 
-    params = {"query": "아이폰", "display": "100", "start": str(start_num)}
+    params = {"query": "베르나르 베르베르", "display": "100", "start": str(start_num)}
     r = requests.get(url, headers=headers, params=params)
     # print(r.url)
 
     # json 가져오기
     data = r.json()
     # print(data)
-    # print(data["items"])
     for idx, item in enumerate(data["items"], 1):
-        # print(idx, item["title"], item["link"])
-        title = re.sub("<.*?>", "", item["title"])
-        ws.append([num, title, item["link"]])
-        num += 1
+        print(idx + num, item["isbn"], item["title"], item["discount"])
+        ws.append([idx + num, item["isbn"], item["title"], item["discount"]])
+    num += 100
 
 base_dir = "./crawl/file/"
-wb.save(base_dir + "naver.xlsx")
+wb.save(base_dir + "베르나르.xlsx")
 wb.close()
